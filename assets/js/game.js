19fe4e8d104c71cf06b1db8bd4153ef969a9bd37
Tyler <tyler.rodgers74@gmail.com>
2022-06-13 13:09:17 -0400

    added console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,26 @@
-window.alert("this is an alert! JavaScript is running!")
+// This creates a function named "fight"
+
+function fight() {
+
+    window.alert("The fight has begun!");
+
+}
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName)
+
+console.log("This logs a string, good for leaving yourself a message")
+
+//This will do math and log 20
+
+console.log(10+10);
+
+// What is this?
+
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+// fight();
