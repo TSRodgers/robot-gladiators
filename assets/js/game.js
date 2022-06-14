530ddcfdb99a2aee520655a2df67bb5b3776be75
Tyler <tyler.rodgers74@gmail.com>
2022-06-14 13:21:32 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,27 +71,65 @@ var fight = function(enemyName) {
              } // end of while loop
             }; // end of fight function 
 
+// function to start a new game
+var startGame = function() {
+    //rest player stats 
+    playerHealth = 100;
+    playerAttack =10;
+    playerMoney = 10;
 
-for(var i = 0; i < enemyNames.length; i++) {
-    // let player know what round they are in
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
-    } else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+    for(var i = 0; i < enemyNames.length; i++) {
+        // let player know what round they are in
+         if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+    
+
+            // pick new enemy to fight backed on the index of the enemyNames array
+            var pickedEnemyName = enemyNames[i];
+    
+             // reset enemyhealt before starting a new fight
+            enemyHealth = 50;
+
+            // use debugger to pause the script fromm running and check what's going on in the code 
+            // debugger
+
+            // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+             fight(pickedEnemyName);
+        }
+
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+         }
     }
 
-    // pick new enemy to fight backed on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
-    
-    // reset enemyhealt before starting a new fight
-    enemyHealth = 50;
+    // after loop ends, player is either out of health or enemies to fight, so run the endGame function 
+    endGame();
+};
 
-    // use debugger to pause the script fromm running and check what's going on in the code 
-    // debugger
+// function to end the entire game 
+var endGame = function() {
+    // if player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".")
+    }
+    else {
+        window.alert("You've lost your robot in battle.")
+    }
 
-    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(enemyNames[i]);
+// ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+    // restart the game
+    startGame();
+}
+else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!")
 }
 
+};
+
+// start the game when the page loads 
+startGame();
 
